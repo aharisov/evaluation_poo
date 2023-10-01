f71986a170bae753c9b6e86880f1a6aab2cb3e6e
Aleksandr Harisov <bwb-a.harisov@fondespierre.com>
2023-10-01 15:18:40 +0200

    initialized delivery of animals

--- helpers.php
+++ helpers.php
@@ -8,10 +8,10 @@ function generateNum($min, $max) {
     return $result;
 }
 
-// une fonction pour générer l'initial nombre des animaux dans le Zoo
-function initialGenereAnimaux($animauxCarnivores, $animauxHerbivores) {
-    $nombreCarnivore = generateNum(5, 15); // nombre des animaux pour générer
-    $nombreHerbivore = generateNum(5, 15);
+// une fonction pour générer l'initial nombre des animaux et nombre pendant un livraison dans le Zoo
+function genereAnimaux($animauxCarnivores, $animauxHerbivores, $minNombre, $maxNombre, $titres = []) {
+    $nombreCarnivore = generateNum($minNombre, $maxNombre); // nombre des animaux pour générer
+    $nombreHerbivore = generateNum($minNombre, $maxNombre);
     $animaux = []; // un tableau pour stocker les animaux
 
     // génère un genre
@@ -23,14 +23,24 @@ function initialGenereAnimaux($animauxCarnivores, $animauxHerbivores) {
 
     // génère les carnivores
     for ($i = 1; $i <= $nombreCarnivore; $i++) {
-        $animalCarnivore = new Carnivore("", $animauxCarnivores[generateNum(1, count($animauxCarnivores) - 1)], $genre);
+        if (count($titres) > 0) {
+            // génère un animal avec un nom graçe à livraison
+            $animalCarnivore = new Carnivore($titres[generateNum(0, count($titres) - 1)], $animauxCarnivores[generateNum(0, count($animauxCarnivores) - 1)], $genre);    
+        } else {
+            $animalCarnivore = new Carnivore("", $animauxCarnivores[generateNum(0, count($animauxCarnivores) - 1)], $genre);
+        }
 
         $animaux[] = $animalCarnivore;
     }
 
     // génère les herbivores
     for ($i = 1; $i <= $nombreHerbivore; $i++) {
-        $animalHerbivore = new Herbivore("", $animauxHerbivores[generateNumber(1, count($animauxHerbivores) - 1)], $genre);
+        if (count($titres) > 0) {
+            // génère un animal avec un nom graçe à livraison
+            $animalHerbivore = new Herbivore($titres[generateNum(0, count($titres) - 1)], $animauxHerbivores[generateNumber(1, count($animauxHerbivores) - 1)], $genre);
+        } else {
+            $animalHerbivore = new Herbivore("", $animauxHerbivores[generateNumber(1, count($animauxHerbivores) - 1)], $genre);
+        }
 
         $animaux[] = $animalHerbivore;
     }
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +10,7 @@
         ini_set('display_startup_errors', '1');
         error_reporting(E_ALL);
 
+        // inclut les necessaires classes et fonctions
         require("class/Zoo.php");
         require("helpers.php");
 
@@ -17,28 +18,39 @@
         $minBilletsVendus = 5;
         // les visiteurs par default
         $visiteurs = ["Loïc", "Yannick", "Yassine", "Enzo", "Pierre"];
-
+        // un tableau pour générer carnivores
         $animauxCarnivores = ["Belettes", "Cachalot", "Caracal", "Carcajou", "Céraste", "Chacal", "Chat", "Chat sauvage", "Chat viverrin", "Chaus", "Coyote", "Dauphin", "Dingo", "Elephant de mer", "Furets", "Fouines", "Glouton", "Guépard", "Hérissons", "Hermines", "Hyène", "Jaguar", "Kodiak", "Léopard", "Léopard des neiges", "Limule", "Lion", "Lion blanc", "Lion de mer", "Loup", "Loutre", "Ocelot", "Opossum", "Panthère", "Pékan", "Phoque", "Puma", "Renards", "Renard arctique", "Serval", "Suricate", "Tigre", "Visons", "Zorille"];
-
+        // un tableau pour générer herbivores
         $animauxHerbivores = ["Alpaga", "Antilope", "Bisons","Capybara", "Chameau", "Cheval", "Chèvre de montagne", "Chinchilla", "Dromadaires", "Écureuil", "Eléphant", "Gaufre", "Gazelle", "Girafe", "Gnou", "Gorille", "Hippopotame", "Kangourou", "Koala", "Lama", "Lapin", "Marmotte", "Mouton", "Panda", "Rhinocéros", "Tapir", "Zèbre"];
-
+        // un tableau pour générer des noms pour des animaux
         $animauxNoms = ["LILI", "SOCCER", "NOOKIE", "METRO", "BOSKO", "BACA", "JUANI", "YOO-RI", "FANCY", "FRIZZY", "OUIZZY", "RAMSAY", "DEYSI", "CARLEY", "DARRYL", "DIVI", "DORI", "CHYLA", "NOVI", "NAKOA", "DASHEL", "BABEAR", "FOSI", "SALINA", "KYLI", "COBEY", "TITI", "WASABI", "TIBPI", "TENBY", "CUSHI", "SUZUKI", "NESSEY", "BLING", "KANCHO", "PADRIC", "CHEWEY", "CEDY", "ANKO", "LAMB"]; 
 
-        // pour stocker les animaux
-        $animaux = initialGenereAnimaux($animauxCarnivores, $animauxHerbivores);
+        // un tableau pour stocker les animaux
+        $animaux = genereAnimaux($animauxCarnivores, $animauxHerbivores, 5, 15);
     ?>
 </head>
 <body>
     <?php 
         // crée une instance de la classe Zoo
         $zoo = new Zoo($animaux, $visiteurs);
+        //var_dump($zoo);
+        // vendre des billets
         $billetsVendus = $zoo->vendreBillet($minBilletsVendus);
+        // génère nombre des animaux après la livraison
+        $animauxLivres = genereAnimaux($animauxCarnivores, $animauxHerbivores, 2, 5, $animauxNoms);
+        // obtien nombre des animaux livrés
+        $nombreLivres = count($animauxLivres);
+        // ajoute les nouvaux animaux dans tableau
+        $animaux = array_merge($animaux, $animauxLivres);
+        // change le tableau des animaux dans le Zoo
+        $zoo->animaux = $animaux;
     ?>
 
     <main>
         <?php
             if ($zoo->ouvreStatus === true) {
                 echo "<h1>Bienvenue dans notre zoo !</h1>";
+                echo $zoo->livraison($nombreLivres);
             } else {
                 echo "<h1>Le Zoo est fermé maintenant.<br>Malheureusement, nous avons vendu juste $billetsVendus billet(s).<br>Reviens une autre fois, s'il vous plaît.</h1>";
             }
