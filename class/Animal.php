98257dc225d27ca7a691729fc5a21f69108fd999
Aleksandr Harisov <bwb-a.harisov@fondespierre.com>
2023-10-01 09:29:08 +0200

    created class herbivore

--- class/Animal.php
+++ class/Animal.php
@@ -3,12 +3,21 @@
 abstract class Animal {
     // stocke le nom de l'animal s'il y a
     public $nom = "";
+
     // stocke le genre de l'animal - mâle ou femelle
     public $genre = "";
 
+    // stocke le titre de l'animal - lion, zebra, etc
+    public $titre = "";
+
+    // stocke le type de l'animal - herbivoe ou carnivore
+    public $type = "";
+
     // la foction constructeur
-    public function __construct($nom, $genre = "mâle") {
+    public function __construct($nom, $titre, $type, $genre = "mâle") {
         $this->nom = $nom;
+        $this->titre = $titre;
+        $this->type = $type;
         $this->genre = $genre;
     }
 
---
+++ class/Herbivore.php
@@ -0,0 +1,44 @@
+<?php
+// inclut la classe parent
+require("Animal.php");
+
+class Herbivore extends Animal {
+    // le type d'animal
+    public $type = ""; 
+    
+    // herite des propriétés de la classe Animal
+    public function __construct($nom, $titre, $type, $genre = "mâle") {
+        parent::__construct($nom, $titre, $type, $genre);
+    }
+
+    // herite de la methode de la classe Animal
+    // retourne le show
+    public function faireLeShow() {
+        // s'il y a le nom
+        if ($this->nom = "") {
+            return "<p>Cet animal $this->type qui est $this->titre et qui s'appelle $this->nom.</p>";
+        }
+
+        // s'il n'y a pas de nom
+        return "<p>Cet animal $this->type qui est $this->titre.</p>";
+    }
+
+    // herite de la methode de la classe Animal
+    // retourne un nouvel animal si c'est possible
+    public function donnerNaissance() {
+        // génère genre du nouvel animal
+        $newGenre = "";
+        $randomNum = rand(0, 1);
+
+        if ($randomNum == 0) {
+            $newGenre = "mâle";
+        } else {
+            $newGenre = "femelle";
+        }
+
+        // retourne un nouvel animal
+        if ($this->genre === "femelle") {
+            return new Herbivore("", $this->titre, $this->type, $newGenre);
+        }
+    }
+}
