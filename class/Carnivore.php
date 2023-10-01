10b37bc084a2151b3a27a46034b0ead35717191d
Aleksandr Harisov <bwb-a.harisov@fondespierre.com>
2023-10-01 13:02:27 +0200

    fixed mistype

--- class/Carnivore.php
+++ class/Carnivore.php
@@ -1,6 +1,7 @@
 <?php
 require("Herbivore.php");
 
+// hérite toutes les propriétés et methodes des classes Animal et Herbivore
 class Carnivore extends Herbivore {
     public function __construct($nom, $titre, $genre = "mâle", $type = "carnivore") {
         parent::__construct($nom, $titre, $genre, $type);
